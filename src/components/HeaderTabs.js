8d753fd19a124b79a998ec3eee2f79984752df40
Saul Ceja <saulceja08@gmail.com>
2023-07-23 16:41:02 -0700

    new support function to upload user concerns and append to db.json

--- src/components/HeaderTabs.js
+++ src/components/HeaderTabs.js
@@ -1,11 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SearchBar from './SearchBar';
 import Games from './Games';
-import Consoles from './Games';
+import Consoles from './Consoles';
 import Support from './Support';
 
-function HeaderTabs({gameData}) {
+function HeaderTabs({ gameData }) {
   const imageUrl = 'https://logolook.net/wp-content/uploads/2021/08/Call-of-Duty-Logo-2011.png';
+
+  // Step 1: Add state variables to manage the form data
+  const [showSupportBox, setShowSupportBox] = useState(false);
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [comment, setComment] = useState('');
+
+  // Step 3: Handle form submission and data appending
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Perform any necessary validation here before appending the data
+    const formData = {
+      name,
+      email,
+      comment,
+    };
+    // Here you can use a suitable method like fetch() to post the data to the server
+    // Example using fetch:
+    fetch('http://localhost:3000/games/support', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('Data submitted successfully:', data);
+        // Do something with the response if needed
+      })
+      .catch((error) => {
+        console.error('Error submitting data:', error);
+      });
+    // Clear the form after submission
+    setName('');
+    setEmail('');
+    setComment('');
+    // Hide the support box after submission
+    setShowSupportBox(false);
+  };
 
   return (
     <div className='tab-list'>
@@ -13,10 +53,21 @@ function HeaderTabs({gameData}) {
         <img src={imageUrl} alt='Tab Icon' />
       </div>
       <div className="tabs">
-        <SearchBar gameData={gameData} placeholder="Search for ..."/>
-        <Games/>
-        <Consoles/>
-        <Support/>
+        <SearchBar gameData={gameData} placeholder="Search for ..." />
+        <Games />
+        <Consoles />
+        {/* Step 2: Pass props and handlers to the Support component */}
+        <Support
+          showSupportBox={showSupportBox}
+          setShowSupportBox={setShowSupportBox}
+          name={name}
+          setName={setName}
+          email={email}
+          setEmail={setEmail}
+          comment={comment}
+          setComment={setComment}
+          handleSubmit={handleSubmit}
+        />
       </div>
     </div>
   );
--- src/components/Support.js
+++ src/components/Support.js
@@ -1,9 +1,66 @@
-import React from 'react'
+import React from 'react';
 
-function Support() {
+function Support({
+  showSupportBox,
+  setShowSupportBox,
+  name,
+  setName,
+  email,
+  setEmail,
+  comment,
+  setComment,
+  handleSubmit,
+}) {
+  if (!showSupportBox) {
+    return (
+      <div className="support-button" onClick={() => setShowSupportBox(true)}>
+        Support
+      </div>
+    );
+  }
+
   return (
-    <div className='tab'>Support+</div>
-  )
+    <div className="support-box">
+      <h2>Support</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="comment">Comment:</label>
+          <textarea
+            id="comment"
+            value={comment}
+            onChange={(e) => setComment(e.target.value)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <button type="submit">Submit</button>
+          <button type="button" onClick={() => setShowSupportBox(false)}>
+            Cancel
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
-export default Support
+export default Support;
