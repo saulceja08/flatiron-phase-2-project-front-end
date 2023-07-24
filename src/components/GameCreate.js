42b12cd81d7fa59580734468acdf1eeb13f3ab07
Saul Ceja <saulceja08@gmail.com>
2023-07-23 20:45:03 -0700

    GameDetails component added into render details

--- src/components/GameCreate.js
+++ src/components/GameCreate.js
@@ -1,9 +1,111 @@
-import React from 'react'
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
-function GameCreate() {
+const GameCreate = () => {
+  const [id, setId] = useState("");
+  const [name, setName] = useState("");
+  const [dateReleased, setDateReleased] = useState("");
+  const [image, setImage] = useState("");
+  const [validation, setValidation] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const gameData = { id, name, dateReleased, image };
+
+    fetch("http://localhost:3000/employee", {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify(gameData),
+    })
+      .then((res) => {
+        alert("Saved successfully.");
+        navigate("/");
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
+
   return (
-    <div>GameCreate</div>
-  )
-}
+    <div>
+      <div className="row">
+        <div className="offset-lg-3 col-lg-6">
+          <form className="container" onSubmit={handleSubmit}>
+            <div className="card" style={{ textAlign: "left" }}>
+              <div className="card-title">
+                <h2>Game Create</h2>
+              </div>
+              <div className="card-body">
+                <div className="row">
+                  <div className="col-lg-12">
+                    <div className="form-group">
+                      <label>ID</label>
+                      <input
+                        value={id}
+                        onChange={(e) => setId(e.target.value)}
+                        className="form-control"
+                      />
+                    </div>
+                  </div>
 
-export default GameCreate
+                  <div className="col-lg-12">
+                    <div className="form-group">
+                      <label>Name</label>
+                      <input
+                        required
+                        value={name}
+                        onMouseDown={(e) => setValidation(true)}
+                        onChange={(e) => setName(e.target.value)}
+                        className="form-control"
+                      />
+                      {name.length === 0 && validation && (
+                        <span className="text-danger">Enter the name</span>
+                      )}
+                    </div>
+                  </div>
+
+                  <div className="col-lg-12">
+                    <div className="form-group">
+                      <label>Date Released</label>
+                      <input
+                        value={dateReleased}
+                        onChange={(e) => setDateReleased(e.target.value)}
+                        className="form-control"
+                      />
+                    </div>
+                  </div>
+
+                  <div className="col-lg-12">
+                    <div className="form-group">
+                      <label>Image URL</label>
+                      <input
+                        value={image}
+                        onChange={(e) => setImage(e.target.value)}
+                        className="form-control"
+                      />
+                    </div>
+                  </div>
+
+                  <div className="col-lg-12">
+                    <div className="form-group">
+                      <button className="btn btn-success" type="submit">
+                        Save
+                      </button>
+                      <Link to="/" className="btn btn-danger">
+                        Back
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default GameCreate;
--- src/components/GameDetails.js
+++ src/components/GameDetails.js
@@ -1,9 +1,51 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
 
-function GameDetails() {
+const GameDetail = () => {
+  const { gameid } = useParams();
+
+  const [gamedata, gamedatachange] = useState({});
+
+  useEffect(() => {
+    fetch("http://localhost:3000/games/" + gameid)
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error('Network response was not OK');
+        }
+        return res.json();
+      })
+      .then((resp) => {
+        gamedatachange(resp);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, [gameid]);
+
   return (
-    <div>GameDetails</div>
-  )
-}
+    <div>
+      <div className="container">
+        <div className="card row" style={{ textAlign: "left" }}>
+          <div className="card-title">
+            <h2>Game Details</h2>
+          </div>
+          <div className="card-body">
+            {Object.keys(gamedata).length !== 0 ? ( // Check if gamedata is not empty
+              <div>
+                <h2>Game Name: <b>{gamedata.name}</b> ({gamedata.id})</h2>
+                <h3>Details</h3>
+                <h5>Release Date: {gamedata.dateReleased}</h5> {/* Correct the property name */}
+                <h5>Image: {gamedata.image}</h5>
+                <Link className="btn btn-danger" to="/">Back to Listing</Link>
+              </div>
+            ) : (
+              <p>Loading game data...</p>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default GameDetails
+export default GameDetail;
